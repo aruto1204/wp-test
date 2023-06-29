23f5df5eac1030121d26a755e02f837f9354688b
ryoma <116142999+aruto1204@users.noreply.github.com>
2023-06-29 13:18:18 +0900

    string型の修正

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 
 export default function Avatar({ author }) {
   const name = author?.node.name;
+  const avatarUrl = author?.node.avatar?.url;
 
   return (
     <div className="flex items-center">
-      <div className="w-12 h-12 relative mr-4">{author?.node.avatar.url ? <Image src={author?.node.avatar.url} fill priority="high" className="rounded-full" alt={name} /> : <p>noImage</p>}</div>
+      <div className="w-12 h-12 relative mr-4">{avatarUrl ? <Image src={avatarUrl} layout="fill" objectFit="cover" className="rounded-full" alt={name} /> : <p>noImage</p>}</div>
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
