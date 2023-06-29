80fb442fc73155d830c073f7c141695672472ab1
ryoma <116142999+aruto1204@users.noreply.github.com>
2023-06-29 15:53:53 +0900

    Image修正

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -6,7 +6,7 @@ export default function Avatar({ author }) {
 
   return (
     <div className="flex items-center">
-      <div className="w-12 h-12 relative mr-4">{avatarUrl ? <Image src={avatarUrl} layout="fill" objectFit="cover" className="rounded-full" alt={name} /> : <p>noImage</p>}</div>
+      <div className="w-12 h-12 relative mr-4">{avatarUrl ? <Image src={avatarUrl} fill className="rounded-full" alt={name} /> : <p>noImage</p>}</div>
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
