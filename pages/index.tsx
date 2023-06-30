8db45870bbb8023813288683ce639970e877dc90
ryoma <116142999+aruto1204@users.noreply.github.com>
2023-06-30 15:38:18 +0900

    カスタム投稿が反映出来るかテスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,10 @@ import Layout from "../components/layout";
 import { getAllPostsForHome } from "../lib/api";
 import { CMS_NAME } from "../lib/constants";
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node;
-  const morePosts = edges.slice(1);
+export default function Index({ allPosts: { newsPosts }, preview }) {
+  const heroPost = newsPosts.edges[0]?.node;
+  const morePosts = newsPosts.edges.slice(1);
+  console.log(morePosts.length);
 
   return (
     <Layout preview={preview}>
